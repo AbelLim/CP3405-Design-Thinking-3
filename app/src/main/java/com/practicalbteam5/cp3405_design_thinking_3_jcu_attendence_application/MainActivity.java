bab119511d17a49d13b1ee813e86d1ff47a2f0f6
AbelLim <abel.lim@my.jcu.edu.au>
2018-12-02 19:01:25 +0800

    Initial commit

---
+++ app/src/main/java/com/practicalbteam5/cp3405_design_thinking_3_jcu_attendence_application/MainActivity.java
@@ -0,0 +1,13 @@
+package com.practicalbteam5.cp3405_design_thinking_3_jcu_attendence_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/practicalbteam5/cp3405_design_thinking_3_jcu_attendence_application/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.practicalbteam5.cp3405_design_thinking_3_jcu_attendence_application;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
